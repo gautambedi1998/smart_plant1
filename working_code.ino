9c9aee9c0d9a05c1df853a4d0b5fa596c1047910
gautambedi1998 <41571537+gautambedi1998@users.noreply.github.com>
2018-10-13 13:06:52 +1100

    Add files via upload

---
+++ working_code.ino
@@ -0,0 +1,30 @@
+int soundSensor=2;
+int LED=4;
+boolean LEDStatus=false;
+
+void setup() {
+  Serial.begin(9600);
+ pinMode(soundSensor,INPUT);
+ pinMode(LED,OUTPUT);
+
+}
+
+void loop() {
+
+  int SensorData=digitalRead(soundSensor); 
+     Serial.println( SensorData);
+  if(SensorData==1){
+
+    if(LEDStatus==false){
+        LEDStatus=true;
+        digitalWrite(LED,HIGH);
+        {
+         Serial.println( SensorData);
+        }
+    }
+    else{
+        LEDStatus=false;
+        digitalWrite(LED,LOW);
+    }
+  }
+ } 
